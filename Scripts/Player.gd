e357638d8596929320657ab76fcae05d7ccc8ba4
aedwards444 <100166222+aedwards444@users.noreply.github.com>
2022-08-13 08:03:52 -0500

    Add files via upload

---
+++ Scripts/Player.gd
@@ -0,0 +1,44 @@
+extends KinematicBody2D
+
+
+var moveSpeed : int = 100
+var jumpForce : int = 300
+var gravity : int = 800
+
+var vel : Vector2 = Vector2()
+var grounded : bool = false
+
+onready var sprite = $Sprite
+
+func _ready():
+	pass # Replace with function body.
+
+func _physics_process(delta):
+	#Reset Horizontal Velocity
+	vel.x = 0
+	
+	#Movement Inputs
+	
+	if Input.is_action_pressed("move_left"):
+		vel.x -= moveSpeed
+	if Input.is_action_pressed("move_right"):
+		vel.x += moveSpeed
+	
+	#applying the Velocity
+	vel = move_and_slide(vel, Vector2.UP)
+	
+	#gravity
+	vel.y += gravity * delta
+	
+	# jump inpout
+	if Input.is_action_pressed("jump") and is_on_floor():
+		vel.y -= jumpForce
+	
+	# sprite direction
+	if vel.x < 0:
+		sprite.flip_h = true
+	elif vel.x > 0:
+		sprite.flip_h = false
+
+func die ():
+	get_tree().reload_current_scene()
